902cf6a83535a36cd98f75612af22712454534e7
Grant Lin <grant.lin@42-q.com>
2019-09-04 14:00:00 +0800

    增加数据库信息配置

---
+++ server/src/main/java/work/jianhang/server/repository/CarRepository.java
@@ -0,0 +1,9 @@
+package work.jianhang.server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import work.jianhang.server.entity.Car;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+}
