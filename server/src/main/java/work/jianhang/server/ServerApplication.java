2c3006f698580a8024027c403c45502a0c99e39d
Grant Lin <grant.lin@42-q.com>
2019-09-04 13:58:43 +0800

    增加.gitignore

--- server/src/main/java/work/jianhang/server/ServerApplication.java
+++ server/src/main/java/work/jianhang/server/ServerApplication.java
@@ -1,7 +1,13 @@
 package work.jianhang.server;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import work.jianhang.server.entity.Car;
+import work.jianhang.server.repository.CarRepository;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +16,16 @@ public class ServerApplication {
         SpringApplication.run(ServerApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner init(CarRepository carRepository) {
+        return args -> {
+            Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+                    "AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+                Car car = new Car();
+                car.setName(name);
+                carRepository.save(car);
+            });
+            carRepository.findAll().forEach(System.out::println);
+        };
+    }
 }
