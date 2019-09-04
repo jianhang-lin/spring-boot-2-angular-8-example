7a031b88babf25ffe56d5ac32a30db962dde5123
Grant Lin <grant.lin@42-q.com>
2019-09-04 15:50:19 +0800

    处理跨域问题

--- server/src/main/java/work/jianhang/server/ServerApplication.java
+++ server/src/main/java/work/jianhang/server/ServerApplication.java
@@ -3,10 +3,16 @@ package work.jianhang.server;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import work.jianhang.server.entity.Car;
 import work.jianhang.server.repository.CarRepository;
 
+import java.util.Collections;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -30,5 +36,19 @@ public class ServerApplication {
             });
             carRepository.findAll().forEach(System.out::println);
         };
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(Boolean.TRUE);
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
