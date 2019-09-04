7d127c1bcaeba082cc3d8a839310a8d94cf5d8e9
Grant Lin <grant.lin@42-q.com>
2019-09-04 14:13:02 +0800

    增加控制器

---
+++ server/src/main/java/work/jianhang/server/controller/CoolCarController.java
@@ -0,0 +1,30 @@
+package work.jianhang.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import work.jianhang.server.entity.Car;
+import work.jianhang.server.repository.CarRepository;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@RestController
+public class CoolCarController {
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @GetMapping("/cool-cars")
+    public Collection<Car> coolCars() {
+        return carRepository.findAll().stream().filter(this::isCool).collect(Collectors.toList());
+    }
+
+    private boolean isCool(Car car) {
+        return !Objects.equals(car.getName(), "AMC Gremlin") &&
+                !Objects.equals(car.getName(), "Triumph Stag") &&
+                !Objects.equals(car.getName(), "Ford Pinto") &&
+                !Objects.equals(car.getName(), "Yugo GV");
+    }
+}
