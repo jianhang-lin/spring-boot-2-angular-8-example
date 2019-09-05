9ed5fdc6df2e9a32ed45621a2cb45c913cdc42c8
Grant Lin <grant.lin@42-q.com>
2019-09-05 11:39:44 +0800

    增加okta的安全配置

---
+++ server/src/main/java/work/jianhang/server/config/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package work.jianhang.server.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+        Okta.configureResourceServer401ResponseBody(http);
+    }
+}
